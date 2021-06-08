6601d6eca7e122f01d475e152cbc71b2ee831fc9
rvo <mail@richardvo.de>
2021-06-08 10:40:50 +0200

    Added icon component

---
+++ src/components/atoms/Icon/icon.stories.tsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import { Meta } from '@storybook/react'
+import IconComponent , { IconProps } from './icon'
+import * as BootstrapIconsSet from '@styled-icons/bootstrap'
+import * as FaIconsSet from '@styled-icons/fa-solid'
+import * as CryptoIconsSet from '@styled-icons/crypto'
+
+export default {
+    title: 'Atom/Icon',
+    component: IconComponent,
+    argTypes: {
+        size: {
+            control: 'number',
+        },
+        color: {
+            control: 'color',
+        },
+        title: {
+            control: 'text',
+        },
+    },
+} as Meta
+
+const buildIcons = (iconSet, props) => (
+    <div className="row">
+        {Object.keys(iconSet).map((key) => (
+            <div className="col-2">
+                <div className="m-4 d-flex align-items-center flex-column">
+                    <IconComponent
+                        key={key}
+                        component={iconSet[key]}
+                        className="mb-3"
+                        {...props}
+                    />
+                    <small className="text-muted">
+                        {iconSet[key].displayName}
+                    </small>
+                </div>
+            </div>
+        ))}
+    </div>
+)
+
+const BootstrapTemplate: (args: IconProps) => JSX.Element = (args: IconProps) => (
+    <div>
+        <h3>Bootstrap Icons</h3>
+        {buildIcons(BootstrapIconsSet, args)}
+    </div>
+)
+
+const CryptoTemplate: (args: IconProps) => JSX.Element = (args: IconProps) => (
+    <div>
+        <h3>Crypto Icons</h3>
+        {buildIcons(CryptoIconsSet, args)}
+    </div>
+)
+
+
+const FontawesomeTemplate: (args: IconProps) => JSX.Element = (args: IconProps) => (
+    <div>
+        <h3>Fontawesome Icons</h3>
+        {buildIcons(FaIconsSet, args)}
+    </div>
+)
+
+
+export const Bootstrap = BootstrapTemplate.bind({})
+Bootstrap.args = { size: 25, color: 'black' }
+
+export const Fontawesome = FontawesomeTemplate.bind({})
+Fontawesome.args = { size: 25, color: 'black' }
+
+export const Crypto = CryptoTemplate.bind({})
+Crypto.args = { size: 45, color: 'black' }
+
+
+
---
+++ src/components/atoms/Icon/icon.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { StyledIcon, StyledIconProps } from '@styled-icons/styled-icon'
+
+export interface IconProps extends StyledIconProps {
+    component: StyledIcon
+    color?: string;
+    className?: string;
+}
+
+const Icon: React.FC<IconProps> = ({ component, children, ...restProps}) => {
+    const IconComponent = component
+
+    return (
+        <IconComponent {...restProps}>
+            {children}
+        </IconComponent>
+    )
+}
+
+export default Icon
