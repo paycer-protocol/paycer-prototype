d9f83b08b658eacda713a231a13590bbe3183fb5
rvo <mail@richardvo.de>
2021-06-10 14:57:27 +0200

    Pagination component added.

---
+++ src/components/molecules/pagination/pagination.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import BasePagination, { PaginationProps as BasePaginationProps } from 'react-bootstrap/Pagination'
+import BasePageItem, { Ellipsis, First, Last, Next, Prev } from 'react-bootstrap/PageItem'
+import './pagination.styles.scss'
+
+export interface PaginationProps extends BasePaginationProps {}
+
+export interface PageItemProps {
+    active?: boolean
+    disabled?: boolean
+    children?: any
+}
+
+const Pagination = (props: PaginationProps) => <BasePagination {...props} />
+export const PageItem = ({ children, ...props}: PageItemProps) => (
+    <BasePageItem {...props}>{children}</BasePageItem>
+)
+
+Pagination.Item = PageItem
+Pagination.Ellipsis = Ellipsis
+Pagination.First = First
+Pagination.Last = Last
+Pagination.Next = Next
+Pagination.Prev = Prev
+
+export default Pagination
