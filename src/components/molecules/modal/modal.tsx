86570c30e44c3c4fd9bccfa8185f67a87a70e4e3
rvo <mail@richardvo.de>
2021-06-10 16:56:03 +0200

    Modal component added.

---
+++ src/components/molecules/modal/modal.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import BaseModal, { ModalProps as BaseModalProps } from 'react-bootstrap/Modal'
+import ModalBody from 'react-bootstrap/ModalBody'
+import ModalContext from 'react-bootstrap/ModalContext'
+import ModalDialog from 'react-bootstrap/ModalDialog'
+import ModalTitle from 'react-bootstrap/ModalTitle'
+import ModalFooter from 'react-bootstrap/ModalFooter'
+import ModalHeader from 'react-bootstrap/ModalHeader'
+import './modal.styles.scss'
+
+export interface ModalProps extends BaseModalProps {}
+
+const Modal = (props: ModalProps) => <BaseModal {...props} />
+
+Modal.Body = ModalBody
+Modal.Context = ModalContext
+Modal.Dialog = ModalDialog
+Modal.Title = ModalTitle
+Modal.Header = ModalHeader
+Modal.Footer = ModalFooter
+
+export default Modal
