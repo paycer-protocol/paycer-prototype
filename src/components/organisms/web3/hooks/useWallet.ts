46a41e94ee34da81907e4d6963d233132540e068
rvo <mail@richardvo.de>
2021-06-12 13:46:02 +0200

    Added trezor and ledger connector. Error handling implemented. Use dotenv for configs.

---
+++ src/components/organisms/web3/hooks/useWallet.ts
@@ -0,0 +1,58 @@
+import { useState, useEffect } from 'react'
+import { useEtherBalance, useEthers, shortenIfAddress } from '@usedapp/core'
+import { formatEther } from '@ethersproject/units'
+import { UnsupportedChainIdError} from '@web3-react/core'
+import { NoEthereumProviderError} from '@web3-react/injected-connector'
+
+export default function useWallet() {
+    const { connector, active, activate, account, deactivate } = useEthers()
+    const etherBalance = useEtherBalance(account)
+    const [activatingConnector, setActivatingConnector] = useState(undefined)
+    const [errorMessage, setErrorMessage] = useState(null)
+
+    useEffect(() => {
+        if (activatingConnector && activatingConnector === connector) {
+            setActivatingConnector(undefined)
+        }
+    }, [activatingConnector, connector])
+
+    const handleConnect = async (provider) => {
+        try {
+            const connector = provider.beforeConnect(provider)
+            setErrorMessage(null)
+            setActivatingConnector(connector)
+            await activate(connector, undefined, true)
+        } catch (e) {
+            handleConnectError(provider, e)
+        }
+    }
+
+    const handleConnectError = (provider, error: Error) => {
+        let message
+
+        if (error instanceof NoEthereumProviderError) {
+            message = 'No Ethereum browser extension detected, install MetaMask on desktop or visit from a dApp browser on mobile.'
+        } else if (error instanceof UnsupportedChainIdError) {
+            message = "You're connected to an unsupported network."
+        } else if (provider.rejectedError && error instanceof provider.rejectedError) {
+            message = 'Please authorize this website to access your Ethereum account.'
+        } else {
+            message = 'An unknown error occurred. Please try again.'
+        }
+
+        setActivatingConnector(undefined)
+        setErrorMessage(message)
+    }
+
+    return {
+        address: account,
+        shortenAddress: shortenIfAddress(account),
+        isActive: active,
+        isConnected: !!account,
+        connect: handleConnect,
+        disconnect: () => deactivate(),
+        etherBalance: formatEther(etherBalance || 0),
+        activatingConnector,
+        errorMessage
+    }
+}
--- src/components/organisms/web3/wallet-provider.tsx
+++ src/components/organisms/web3/wallet-provider.tsx
@@ -1,28 +1,21 @@
 import React from 'react'
 import useWallet from './hooks/useWallet'
 import Button from '../../atoms/button'
+import Alert from '../../atoms/alert'
+import Spinner from '../../atoms/spinner'
 import ListGroup from '../../molecules/list-group'
 import Modal from '../../molecules/modal'
+import { Provider } from './providers'
 
-export interface Provider {
-    connector: any
-    name: string
-    description?: string
-    icon: string
-}
 
 export interface WalletProviderProps {
     providers: Provider[]
 }
 
+
 const WalletProvider = (props: WalletProviderProps) => {
     const { providers = [] } = props
     const wallet = useWallet()
-
-
-    const handleConnect = (provider: Provider) => {
-        wallet.connect(provider.connector)
-    }
 
     return (
         <Modal.Dialog>
@@ -30,20 +23,39 @@ const WalletProvider = (props: WalletProviderProps) => {
                 <Modal.Title>Connect to a wallet</Modal.Title>
             </Modal.Header>
             <Modal.Body>
+                <Alert variant="danger" show={!!wallet.errorMessage}>
+                    {wallet.errorMessage}
+                </Alert>
                 <ListGroup>
-                    {providers.map(item => (
-                        <Button key={item.name} variant="outline-dark" className="mb-2" onClick={() => handleConnect(item)}>
-                            <div className="d-flex align-items-center justify-content-between py-3 px-2">
-                                <div className="text-left">
-                                    <strong>{item.name}</strong>
-                                    <p className="text-muted mb-0">
-                                        <small>{item.description}</small>
-                                    </p>
+                    {providers.map(item => {
+                        const isActivating = wallet.activatingConnector && wallet.activatingConnector === item.connector
+                        const isDisabled = wallet.activatingConnector && wallet.activatingConnector !== item.connector
+
+                        return (
+                            <Button
+                                key={item.name}
+                                variant="outline-dark"
+                                className="mb-2"
+                                active={isActivating}
+                                disabled={isDisabled}
+                                onClick={() => wallet.connect(item)}
+                            >
+                                <div className="d-flex align-items-center justify-content-between py-3 px-2">
+                                    <div className="text-left">
+                                        <strong>{item.name}</strong>
+                                        <p className="text-muted mb-0">
+                                            <small>{item.description}</small>
+                                        </p>
+                                    </div>
+                                    {
+                                        isActivating
+                                            ? <Spinner animation="border" />
+                                            : <img src={item.icon} alt={item.name} width="24" height="24" />
+                                    }
                                 </div>
-                                <img src={item.icon} alt={item.name} width="24" height="24" />
-                            </div>
-                        </Button>
-                    ))}
+                            </Button>
+                        )
+                    })}
                 </ListGroup>
             </Modal.Body>
         </Modal.Dialog>
