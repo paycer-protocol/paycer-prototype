2cfd259cd0fcc36c30c44d80925a62d3da442c7d
rvo <mail@richardvo.de>
2021-06-14 18:01:13 +0200

    Removed color, changed color

--- src/components/organisms/layout/layout.stories.tsx
+++ src/components/organisms/layout/layout.stories.tsx
@@ -22,9 +22,7 @@ const Template: Story<StoryOptions> = (props: StoryOptions) => (
     <DAppProvider config={config}>
         <LayoutComponent {...props}>
             <div className="container">
-                <div className="d-flex align-items-center p-3 my-3 text-white-50 bg-purple rounded box-shadow">
-                    <img className="mr-3" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-outline.svg"
-                         alt="" width="48" height="48" />
+                <div className="d-flex align-items-center p-3 my-3 text-white-50 bg-primary rounded box-shadow">
                     <div className="lh-100">
                         <h6 className="mb-0 text-white lh-100">Bootstrap</h6>
                         <small>Since 2011</small>
