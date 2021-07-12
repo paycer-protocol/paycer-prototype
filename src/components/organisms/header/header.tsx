cd4b0a1e96f134767f8fe371a79f2f99c998e077
Lukas Gurschler <lukas.gurschler@gmail.com>
2021-07-12 23:54:48 +0200

    add responsive offcanvas menu

--- src/components/organisms/header/header.tsx
+++ src/components/organisms/header/header.tsx
@@ -3,7 +3,8 @@ import styled from 'styled-components'
 import Link from 'next/link'
 import Image from '@components/atoms/image'
 import Navbar from '@components/molecules/navbar'
-import Nav from '@components/molecules/nav'
+import { useMediaQuery } from 'react-responsive'
+import { slide as Menu } from 'react-burger-menu'
 import Account from '../web3/account'
 
 const StyledBrand = styled(Navbar.Brand)`
@@ -21,6 +22,43 @@ export interface HeaderProps {
 
 const Header = (props: HeaderProps) => {
     const {} = props
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+
+    const renderMenu = () => {
+        return (
+            <ul className="navbar-nav">
+                <li className="nav-item me-3">
+                    <Link href="/">
+                        <a className="nav-link" title="Dashboard">
+                            Dashboard
+                        </a>
+                    </Link>
+                </li>
+                <li className="nav-item me-3">
+                    <Link href="/invest">
+                        <a className="nav-link" title="Dashboard">
+                            Invest
+                        </a>
+                    </Link>
+                </li>
+                <li className="nav-item me-4">
+                    <Link href="/docs">
+                        <a className="nav-link" title="Documentation">
+                            Docs
+                        </a>
+                    </Link>
+                </li>
+                {(!isTabletOrMobile &&
+                  <li className="nav-item">
+                    <Account
+                      buttonVariant="light"
+                      dropdownVariant="light"
+                    />
+                  </li>
+                )}
+            </ul>
+        )
+    }
 
     return (
         <header>
@@ -33,39 +71,31 @@ const Header = (props: HeaderProps) => {
                             </StyledBrand>
                         </a>
                     </Link>
+                    {(isTabletOrMobile
+                        ? (
+                            <>
+                                <div style={{ position: 'absolute', right: 90}}>
+                                    <Account
+                                        buttonVariant="light"
+                                        dropdownVariant="light"
+                                    />
+                                </div>
+                                <div>
+                                    <div style={{ right: 20, top: 20}} className="position-absolute">
+                                        <Menu right>
+                                            {renderMenu()}
+                                        </Menu>
+                                    </div>
+                                </div>
+                            </>
+                        )
+                        : (
+                            <Navbar.Collapse id="basic-navbar-nav">
+                                {renderMenu()}
+                            </Navbar.Collapse>
+                        )
+                    )}
 
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <ul className="navbar-nav me-lg-auto">
-                            <li className="nav-item me-3">
-                                <Link href="/">
-                                    <a className="nav-link" title="Dashboard">
-                                        Dashboard
-                                    </a>
-                                </Link>
-                            </li>
-                            <li className="nav-item me-3">
-                                <Link href="/invest">
-                                    <a className="nav-link" title="Dashboard">
-                                        Invest
-                                    </a>
-                                </Link>
-                            </li>
-                            <li className="nav-item me-4">
-                                <Link href="/docs">
-                                    <a className="nav-link" title="Documentation">
-                                        Docs
-                                    </a>
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Account
-                                    buttonVariant="light"
-                                    dropdownVariant="light"
-                                />
-                            </li>
-                        </ul>
-                    </Navbar.Collapse>
                 </div>
             </Navbar>
         </header>
