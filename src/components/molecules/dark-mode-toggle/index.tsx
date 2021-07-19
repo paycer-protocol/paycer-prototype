61018df581f8bded2b89b45f08020eac31460be4
rvo <mail@richardvo.de>
2021-07-19 20:33:00 +0200

    save changes

--- src/components/molecules/dark-mode-toggle/index.tsx
+++ src/components/molecules/dark-mode-toggle/index.tsx
@@ -5,18 +5,18 @@ import Button from '@components/atoms/button'
 import { Sun, Moon } from '@styled-icons/bootstrap'
 import useDarkMode from '@hooks/use-dark-mode'
 
-export default function DarkModeToggle() {
-
+export default function Index() {
   const darkMode = useDarkMode()
+
   return (
     <div className="btn-group-toggle d-flex align-items-center">
       <Button variant="light" className="w-50 me-2" active={!darkMode.enabled} onClick={() => darkMode.setEnabled(false)}>
-        <div className="d-flex align-items-center p-1" style={{ whiteSpace: 'nowrap' }}>
+        <div className="d-flex align-items-center p-1 text-nowrap">
           <Icon component={Sun} className="me-2" size={18} /> <Trans>Light Mode</Trans>
         </div>
       </Button>
       <Button variant="light" className="w-50" active={darkMode.enabled} onClick={() => darkMode.setEnabled(true)}>
-        <div className="d-flex align-items-center p-1" style={{ whiteSpace: 'nowrap' }}>
+        <div className="d-flex align-items-center p-1 text-nowrap">
           <Icon component={Moon} className="me-2" size={18} /> <Trans>Dark Mode</Trans>
         </div>
       </Button>
