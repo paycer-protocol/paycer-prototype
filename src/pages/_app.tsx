20a2b3d72c062c3e6887e18e4d7d199928b17c0f
rvo <mail@richardvo.de>
2021-07-21 12:53:16 +0200

    wip

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import { AppProps } from 'next/app'
+import Router from 'next/router'
+import NProgress from 'nprogress'
 import { ToastContainer } from 'react-toastify'
 import I18nProvider from '../locales/i18n'
 import Web3Context from '@components/organisms/web3/web3-context'
 import Layout from '@components/organisms/layout'
 import '../../assets/theme-dark.scss'
+
+Router.events.on('routeChangeStart', NProgress.start)
+Router.events.on('routeChangeComplete', NProgress.done)
+Router.events.on('routeChangeError', NProgress.done)
+
 
 const App = ({ Component, pageProps }: AppProps) => (
     <Web3Context>
