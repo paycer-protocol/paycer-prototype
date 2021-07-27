0fec46102e6833fcf02e7002288cd0e374716c3c
Lukas Gurschler <lukas.gurschler@gmail.com>
2021-07-27 10:27:36 +0200

    remove unused code

--- src/components/atoms/blur-background/blur-background.tsx
+++ src/components/atoms/blur-background/blur-background.tsx
@@ -6,13 +6,9 @@ const BlurBackground = styled.div`
     width: 100%;
     height: 100%;
     background: linear-gradient(122deg,#86325591,#229c7a4d);
-    -webkit-backdrop-filter: blur(5px);
-    -webkit-backdrop-filter: blur(5px);
-    backdrop-filter: blur(5px);
-    -webkit-filter: blur(8px);
-    filter: blur(8px);
+    filter: blur(80px);
     -webkit-filter: blur(80px);
-    opacity: 0.78;
+    opacity: 0.85;
 `
 
 export default BlurBackground
