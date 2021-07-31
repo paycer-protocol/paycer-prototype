ec839699dd61c192c1d8a1a0ce5011c477b63983
rvo <mail@richardvo.de>
2021-08-01 00:38:05 +0200

    Portfolio data with strategy types added (#34)

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,12 +1,39 @@
 import React from 'react'
-import Link from 'next/link'
 import { Trans } from '@lingui/macro'
 import PageHeader from '@components/molecules/page-header'
 import DashCards from './components/dash-cards'
 import Portfolio from './components/portfolio'
-import Button from '@components/atoms/button'
+import { StrategyType } from '../../types/investment'
+import {investmentStrategies} from '@config/investment/strategies'
+import useToken from '@hooks/use-token'
+
+interface PortfolioStrategy extends StrategyType {
+  balance?: number
+  tvl?: number
+}
 
 export default function Home() {
+  const qualifiedStrategies = []
+  let totalBalance = 0
+
+  investmentStrategies.map((strategy) => {
+    const token = strategy.output
+    const outputToken = useToken(token.symbol)
+    const tokenBalance = Math.random() * 10000 // outputToken.tokenBalance()
+
+    totalBalance += tokenBalance
+
+    if (tokenBalance > 0) {
+      qualifiedStrategies.push({
+        ...strategy,
+        ...{
+          balance: tokenBalance,
+          tvl: 12712983
+        }
+      } as PortfolioStrategy)
+    }
+  })
+
   return (
     <div className="container mt-3">
       <PageHeader>
@@ -23,11 +50,14 @@ export default function Home() {
       </PageHeader>
       <div className="blur-background">
           <div className="mb-6 mb-md-8">
-            <DashCards />
+            <DashCards totalBalance={totalBalance} />
           </div>
           <div className="row position-relatives">
             <div className="col-12">
-              <Portfolio />
+              <Portfolio
+                strategies={qualifiedStrategies}
+                totalBalance={totalBalance}
+              />
             </div>
           </div>
       </div>
