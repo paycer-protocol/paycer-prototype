af04e21bde41aa1293fafa19d884853ba52ff32a
rvo <mail@richardvo.de>
2021-08-05 22:50:29 +0200

    Added google tag manager

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
+import React, { useEffect } from 'react'
 import { AppProps } from 'next/app'
 import Router from 'next/router'
 import NProgress from 'nprogress'
 import { ToastContainer } from 'react-toastify'
+import TagManager from 'react-gtm-module'
 import I18nProvider from '../locales/i18n'
 import Web3Context from '@components/organisms/web3/web3-context'
 import Layout from '@components/organisms/layout'
@@ -11,16 +13,25 @@ Router.events.on('routeChangeStart', NProgress.start)
 Router.events.on('routeChangeComplete', NProgress.done)
 Router.events.on('routeChangeError', NProgress.done)
 
+const tagManagerArgs = {
+  id: 'G-H24T1CYZ06',
+}
 
-const App = ({ Component, pageProps }: AppProps) => (
+const App = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs)
+  }, [])
+
+  return (
     <Web3Context>
-        <I18nProvider>
-            <Layout>
-                <ToastContainer position="top-center" />
-                <Component {...pageProps} />
-            </Layout>
-        </I18nProvider>
+      <I18nProvider>
+        <Layout>
+          <ToastContainer position="top-center" />
+          <Component {...pageProps} />
+        </Layout>
+      </I18nProvider>
     </Web3Context>
-)
+  )
+}
 
 export default App
