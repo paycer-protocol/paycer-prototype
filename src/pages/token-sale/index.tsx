e66f28a9a499e3514224f12aaba4114b756b05b6
Christian Oellers <2101569+ChristianOellers@users.noreply.github.com>
2021-10-12 17:54:17 +0200

    API implementation - WIP

--- src/pages/token-sale/index.tsx
+++ src/pages/token-sale/index.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Trans } from '@lingui/macro'
 import PageHeader from '@components/molecules/page-header'
 
+export default function TokenSale() {
+  const [apiData, setApiData] = useState({})
 
-export default function Home() {
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(jsonData => setApiData(jsonData))
+      .catch(error => {
+        console.log(error);
+      })
+  }, [])
+
   return (
     <div className="container mt-3 mb-8">
       <PageHeader>
