982d4c9e6ff2b331c4bb939d7d5426b3a5530113
Christian Oellers <2101569+ChristianOellers@users.noreply.github.com>
2021-10-13 11:55:41 +0200

    Dummy markup

--- src/pages/token-sale/index.tsx
+++ src/pages/token-sale/index.tsx
@@ -1,15 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import { Trans } from '@lingui/macro'
+import Button from '@components/atoms/button'
+import Card from '@components/molecules/card'
 import PageHeader from '@components/molecules/page-header'
 
 export default function TokenSale() {
   const [apiData, setApiData] = useState({})
 
   useEffect(() => {
+    // Todo: Replace
     fetch('https://jsonplaceholder.typicode.com/todos/1')
       .then(response => response.json())
       .then(jsonData => setApiData(jsonData))
       .catch(error => {
+        // Todo: Show toast?
         console.log(error);
       })
   }, [])
@@ -20,15 +24,39 @@ export default function TokenSale() {
         <div className="row align-items-center">
           <div className="col">
             <PageHeader.Subtitle>
-              <Trans>Overview</Trans>
+              <Trans>Token Sale</Trans>
             </PageHeader.Subtitle>
             <PageHeader.Title>
-              <Trans>Token Sale</Trans>
+              <Trans>Investor Dashboard</Trans>
             </PageHeader.Title>
           </div>
         </div>
       </PageHeader>
-      <div>
+      <div className="row g-0">
+        <div className="col-lg-5">
+          <Card className="rounded-0">
+            <Card.Body>
+              <Card.Title>Headline</Card.Title>
+              <Card.Text>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Facilis non dolore est fuga nobis ipsum illum eligendi nemo iure repellat,
+                soluta, optio minus ut reiciendis voluptates enim impedit veritatis officiis.
+              </Card.Text>
+              <footer className="d-flex align-items-center justify-content-center">
+                <Button variant="primary">
+                  <Trans>Action</Trans>
+                </Button>
+              </footer>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="col-lg-7">
+          <Card className="rounded-0">
+            <Card.Body>
+              ...
+            </Card.Body>
+          </Card>
+        </div>
       </div>
     </div>
   )
