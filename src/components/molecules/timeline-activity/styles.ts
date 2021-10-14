5038a6edc4e12d7b4f75c800713a70a34208a83f
Christian Oellers <2101569+ChristianOellers@users.noreply.github.com>
2021-10-14 18:03:50 +0200

    Fix icon style

--- src/components/molecules/timeline-activity/styles.ts
+++ src/components/molecules/timeline-activity/styles.ts
@@ -1,6 +1,9 @@
 import styled from 'styled-components'
 
 export const StyledTimeline = styled.div`
+    background: #1B2236;
+    border: 1px solid #3B506C;
+
     svg {
       fill: #3B506C;
     }
