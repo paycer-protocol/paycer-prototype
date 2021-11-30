2c2d67527734be303a58c22f6a4ea0930a42a2b4
rvo <mail@richardvo.de>
2021-11-30 09:39:15 +0100

    wip

--- src/pages/referral/index.tsx
+++ src/pages/referral/index.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 import PageHeader from '@components/molecules/page-header'
 import ReferralUrl from './components/ReferralUrl'
 import Referrals from './components/Referrals'
+import useWallet from '@hooks/use-wallet'
+import Button from '@components/atoms/button'
+import {connectors} from '@providers/connectors'
+import {Trans} from '@lingui/macro'
 
 export default function Referral () {
+  const wallet = useWallet()
 
   return (
     <div className="container mt-3">
@@ -17,8 +22,20 @@ export default function Referral () {
       </PageHeader>
       <div className="card blur-background">
         <div className="card-body">
-          <ReferralUrl />
-          <Referrals />
+          {wallet.isConnected && (
+            <>
+              <ReferralUrl />
+              <Referrals />
+            </>
+          )}
+
+          {!wallet.isConnected && (
+            <div className="d-flex justify-content-center">
+              <Button variant="primary" className="px-5" onClick={() => wallet.connect(connectors[0])}>
+                <Trans>Connect to a Wallet</Trans>
+              </Button>
+            </div>
+          )}
         </div>
       </div>
     </div>
