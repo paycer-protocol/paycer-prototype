ff70230bcbcfe01482419600ee4948157a7081d2
Lukas Gurschler <lukas.gurschler@gmail.com>
2021-12-22 20:29:58 +0100

    PCRDEV-106 loyalty tiers token sale

---
+++ src/components/atoms/tier-level/Styles.tsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+import DropdownComponent from "@components/atoms/dropdown/dropdown";
+
+export const StyledDropdownToggle = styled(DropdownComponent.Toggle)`
+    position: absolute;
+    background: transparent!important;
+    border: 0 none;
+    padding: 0;
+    font-weight: 300;    
+    top: -4px;
+    left: 5px;
+    &:after { display: none }
+    &:focus {
+      box-shadow: none!important;
+    }
+`
+
+export const StyledDropdownMenu = styled(DropdownComponent.Menu)`
+    line-height: 30px;
+    inset: initial!important;
+    top: 0!important;
+    width: 350px;
+    padding: 20px;
+    left: 20px!important;
+    border-width: 1px;
+`
