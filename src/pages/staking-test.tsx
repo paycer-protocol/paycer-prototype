80feddd2e050aff91b2e93024538fff8b06ec572
rvo <mail@richardvo.de>
2021-12-23 02:15:59 +0100

    wip

--- src/pages/staking-test.tsx
+++ src/pages/staking-test.tsx
@@ -1,59 +1,22 @@
-import React, { useState, useEffect } from 'react'
-import { useContractCall, useContractFunction} from '@usedapp/core'
+import React from 'react'
+import { useContractFunction} from '@usedapp/core'
 import { Contract } from '@ethersproject/contracts'
-import Staking from "../deployments/localhost/Staking.json"
+import Staking from "../deployments/kovan/Staking.json"
 import useWallet from '@hooks/use-wallet'
-import { Interface } from '@ethersproject/abi'
-import { BigNumber } from '@ethersproject/bignumber'
 
 export default function StakingTest () {
   const wallet = useWallet()
-  const [rewardAPY, setRewardAPY] = useState(0)
-  const [availableReward, setAvailableReward] = useState(0)
 
   const stakingContract = new Contract(Staking.address, Staking.abi)
   const { send: deposit, state: depositTx } = useContractFunction(stakingContract, 'deposit')
   const { send: withdraw, state: withdrawTx } = useContractFunction(stakingContract, 'withdraw')
   const { send: claim, state: claimTx } = useContractFunction(stakingContract, 'claim')
 
-  const pendingRewardResult = useContractCall({
-      abi: new Interface(Staking.abi),
-      address: Staking.address,
-      method: 'pendingReward',
-      args: [wallet.address]
-  })
-
-  useEffect(() => {
-    if (pendingRewardResult) {
-      setRewardAPY(BigNumber.isBigNumber(pendingRewardResult[0]) ? pendingRewardResult[0].toNumber() : 0)
-    }
-  }, [pendingRewardResult, wallet.address])
-
-
-  const availableRewardResult = useContractCall({
-    abi: new Interface(Staking.abi),
-    address: Staking.address,
-    method: 'availableReward',
-    args: []
-  })
-
-  useEffect(() => {
-    if (availableRewardResult) {
-      setAvailableReward(BigNumber.isBigNumber(availableRewardResult[0]) ? availableRewardResult[0].toNumber() : 0)
-    }
-  }, [availableRewardResult, wallet.address])
-
   return (
     <div className="container">
       <h1>Staking test</h1>
 
       <div className="row">
-        <div className="col-12">
-          <h2>availableReward: {availableReward}</h2>
-        </div>
-        <div className="col-12">
-          <h2>RewardAPY: {rewardAPY}</h2>
-        </div>
         <div className="col-12">
           <h2>Claim status: {claimTx.status}</h2>
           <button onClick={async () => {
