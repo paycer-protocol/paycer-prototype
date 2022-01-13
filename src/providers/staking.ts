ef1044423a263e820e7db92ce94b4c2d0d67a915
rvo <mail@richardvo.de>
2022-01-13 03:11:56 +0100

    wip

--- src/providers/staking.ts
+++ src/providers/staking.ts
@@ -3,6 +3,6 @@ import { ChainId } from '@usedapp/core'
 
 export default {
     abi: StakingAbi,
-    [ChainId.Mumbai]: '0xd62D07B8E29BA0fd9A9299f2D94c604925C5557b',
-    [ChainId.Polygon]: '0xe8d4FA6FA3065B4dB116d54644eFF05F2DD9B268'
+    [ChainId.Mumbai]: '0x5C86297b9759B1994Ab2fAeeE411817c50190Ac5',
+    [ChainId.Polygon]: '0x9F73a9D1777DAb73eb41A29782858f86aA4624B6'
 }
