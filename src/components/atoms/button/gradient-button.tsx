21d542d0ff082a1357acb32ad382e89793fdd507
Lukas Gurschler <lukas.gurschler@gmail.com>
2022-02-10 07:25:27 +0100

    button fixxes

--- src/components/atoms/button/gradient-button.tsx
+++ src/components/atoms/button/gradient-button.tsx
@@ -5,18 +5,18 @@ const GradientButton = styled(Button)`
   background: linear-gradient(86deg, rgba(133, 12, 167, 1) 0%, rgba(66, 1, 220, 1) 100%);
   color: #FFF;
   font-weight: bold;
-  padding: 9px 40px 8px 40px;
+  padding: 11px 40px 10px 40px;
   transition: all;
   border-radius: 25px;
-  text-align: center;
+  text-align: center; border: 0 none;
   ${props => props.isInverted && css`
       background: linear-gradient(86deg, rgba(133, 12, 167, 1) 0%, rgba(66, 1, 220, 1) 100%);
-      padding: 9px 1px;
-      height: 41px;
-       span { 
-       padding: 10px 36px 10px 36px;
-       border-radius: 25px; 
-      }
+         padding: 9px 2px;
+         height: 43px;
+         span { 
+           padding: 11px 36px;
+           border-radius: 25px; 
+        }
   `}
   &:hover {
     background: linear-gradient(86deg, rgb(109, 12, 136) 0%, rgb(59, 4, 189) 100%);
