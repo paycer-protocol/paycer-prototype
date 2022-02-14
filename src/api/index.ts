e445001b989f48c0f719a395186d9d6668e0f107
rvo <mail@richardvo.de>
2022-02-14 04:49:36 +0100

    change api address (#131)

--- src/api/index.ts
+++ src/api/index.ts
@@ -8,22 +8,22 @@ interface PricePair {
 
 export default {
   fetchTokenSaleInfo: (walletAddress: string): Promise<any> => (
-    axios.get(`https://api.paycer.io/token_sales/${walletAddress}`)
+    axios.get(`https://api.paycer.io/v1/token_sales/${walletAddress}`)
   ),
   fetchAllTokenSaleInfo: (walletAddress: string): Promise<any> => (
-      axios.get(`https://api.paycer.io/token_sales?walletAddress=${walletAddress}`)
+      axios.get(`https://api.paycer.io/v1/token_sales?walletAddress=${walletAddress}`)
   ),
   fetchReferralCode: (walletAddress: string): Promise<any> => (
-    axios.get(`https://api.paycer.io/referrals/${walletAddress}`)
+    axios.get(`https://api.paycer.io/v1/referrals/${walletAddress}`)
   ),
   createReferralCode: (walletAddress: string): Promise<any> => (
-    axios.post(`https://api.paycer.io/referrals`, { walletAddress })
+    axios.post(`https://api.paycer.io/v1/referrals`, { walletAddress })
   ),
   fetchReferralRewards: (walletAddress: string): Promise<any> => (
-    axios.get(`https://api.paycer.io/referrals/${walletAddress}/rewards`)
+    axios.get(`https://api.paycer.io/v1/referrals/${walletAddress}/rewards`)
   ),
   fetchPriceChart: async(token0Symbol: string, token1Symbol: string, range: string): Promise<[number, number][]> => (
-      axios.get(`https://api.paycer.io/pair_prices/historical/${token0Symbol}/${token1Symbol}/${range}`).
+    axios.get(`https://api.paycer.io/v1/pair_prices?symbol=${token0Symbol}&base=${token1Symbol}&order[time]=desc`).
       then(res => (res.data as PricePair[]).map(pp => [moment(pp.time).valueOf(), Number(pp.quote)]))
   )
 }
