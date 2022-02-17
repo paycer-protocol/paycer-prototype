28939a185f9e282043d66a93d0984712cbe26bc4
Lukas Gurschler <lukas.gurschler@gmail.com>
2022-02-17 11:32:37 +0100

    remove kovan completely

--- src/config/network.ts
+++ src/config/network.ts
@@ -3,18 +3,15 @@ import {ChainId} from '@usedapp/core'
 export const supportedChains = [
   ChainId.Mainnet,
   ChainId.Polygon,
-  ChainId.Mumbai,
-    ChainId.Kovan
+  ChainId.Mumbai
 ]
 
 export const supportedStakingChains = [
   ChainId.Polygon,
-  ChainId.Mumbai,
-  ChainId.Kovan
+  ChainId.Mumbai
 ]
 
 export const supportedVestingChains = [
   ChainId.Polygon,
-  ChainId.Mumbai,
-  ChainId.Kovan
+  ChainId.Mumbai
 ]
