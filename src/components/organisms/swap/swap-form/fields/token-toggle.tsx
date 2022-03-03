adee2988cb33a838dd12715595ba038d030a0c62
stompudown <39615089+stompudown@users.noreply.github.com>
2022-03-03 09:21:20 +0100

    Task/pcrr 338/swap initial token emptyy (#144)

--- src/components/organisms/swap/swap-form/fields/token-toggle.tsx
+++ src/components/organisms/swap/swap-form/fields/token-toggle.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import styled from 'styled-components'
 import { ArrowDropDown } from '@styled-icons/material-outlined'
 import CurrencyIcon from '@components/atoms/currency-icon'
 import Icon from '@components/atoms/icon'
 import { TokenType } from '../../../../../types/investment'
+import { t } from '@lingui/macro'
 
 interface TokenToggleProps {
   token: TokenType
@@ -15,16 +15,22 @@ export default function TokenToggle(props: TokenToggleProps) {
   const { token, onClick, label } = props
   return (
     <div className="d-flex align-items-center cursor-pointer" onClick={onClick}>
-        <CurrencyIcon
-            symbol={token.symbol}
-            className="me-3"
-            width={32}
-            height={32}
-        />
+        {token &&
+            <CurrencyIcon
+              symbol={token.symbol}
+              className="me-3"
+              width={32}
+              height={32}
+            />
+        }
         <div>
             <small style={{paddingBottom: '1px'}} className="text-muted d-block fw-lighter">{label}</small>
             <div className="d-flex align-items-center">
-                <h3 className="mb-0 text-white">{token.symbol}</h3>
+                {token ?
+                    <h3 className="mb-0 text-white">{token.symbol}</h3>
+                :
+                    t`Select a token`
+                }
                 <Icon
                     component={ArrowDropDown}
                     size={20}
