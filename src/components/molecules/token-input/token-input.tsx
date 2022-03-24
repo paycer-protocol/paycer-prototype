3b2b833dfbc2718c9da578b655b268d6afbfef0a
stompudown <39615089+stompudown@users.noreply.github.com>
2022-03-24 12:50:08 +0100

    Update src/components/molecules/token-input/token-input.tsx
    Co-authored-by: Julien Scholz <julienscholzo@hotmail.de>

--- src/components/molecules/token-input/token-input.tsx
+++ src/components/molecules/token-input/token-input.tsx
@@ -59,7 +59,7 @@ export default function TokenInput(props: TokenInputProps) {
               handleChange(tokenValue)
             }}
         />
-        {(!disabled && balance) &&
+        {(!disabled && balance !== undefined) &&
           <div className="d-flex justify-content-end">
             <TokenBalanceLabel className="text-muted">
               <span>{t`Balance:`}</span>&nbsp;
