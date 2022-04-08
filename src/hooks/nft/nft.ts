70f44fd09fb7bc61aeb4b766aad99e653eb3ad75
Lukas Gurschler <lukas.gurschler@gmail.com>
2022-04-08 22:41:14 +0200

    fix import error

--- src/hooks/nft/nft.ts
+++ src/hooks/nft/nft.ts
@@ -1,5 +1,5 @@
 import { LoyaltyTier } from "@config/loyalty-tiers";
-import { BigNumber } from "ethers";
+import { BigNumber } from '@ethersproject/bignumber'
 
 export default interface Nft {
     id: BigNumber;
