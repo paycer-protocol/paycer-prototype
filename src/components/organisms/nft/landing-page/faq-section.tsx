b5250e75ebe9890b7be976781652b92bd7583e9d
Julien Scholz <8947616+pikaju@users.noreply.github.com>
2022-05-03 09:26:57 +0200

    PN-19 Update FAQ section design

--- src/components/organisms/nft/landing-page/faq-section.tsx
+++ src/components/organisms/nft/landing-page/faq-section.tsx
@@ -1,6 +1,13 @@
 import PageHeader from "@components/molecules/page-header";
 import { Trans } from "@lingui/macro";
 import { Accordion } from "react-bootstrap";
+import styled from "styled-components";
+
+const AccordionItemBorder = styled.div`
+  border: 1px solid #3C506F;
+  border-radius: 8px;
+  margin-bottom: 0.5rem;
+`
 
 export default function FaqSection() {
   return (
@@ -9,30 +16,34 @@ export default function FaqSection() {
       <PageHeader.Title><Trans>We answer your questions</Trans></PageHeader.Title>
 
       <Accordion className="mt-5">
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>What is the total supply?</Accordion.Header>
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-            est laborum.
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="1">
-          <Accordion.Header>Accordion Item #2</Accordion.Header>
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-            est laborum.
-          </Accordion.Body>
-        </Accordion.Item>
+        <AccordionItemBorder>
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>What is the total supply?</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+              est laborum.
+            </Accordion.Body>
+          </Accordion.Item>
+        </AccordionItemBorder>
+        <AccordionItemBorder>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>Accordion Item #2</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+              commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+              est laborum.
+            </Accordion.Body>
+          </Accordion.Item>
+        </AccordionItemBorder>
       </Accordion>
     </div>
   )
