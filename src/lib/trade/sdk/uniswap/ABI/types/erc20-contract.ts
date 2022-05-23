c865e1cb48c0934ed6b36f61537526e253d5f793
Lukas Gurschler <lukas.gurschler@gmail.com>
2022-05-23 12:55:26 +0200

    test

--- src/lib/trade/sdk/uniswap/ABI/types/erc20-contract.ts
+++ src/lib/trade/sdk/uniswap/ABI/types/erc20-contract.ts
@@ -1,5 +1,6 @@
-import { EthersContractContextV5 } from 'ethereum-abi-types-generator';
-import { BigNumber, BigNumberish, ContractTransaction } from 'ethers';
+import { EthersContractContextV5 } from 'ethereum-abi-types-generator'
+//@ts-ignore
+import { BigNumber, BigNumberish, ContractTransaction } from 'ethers'
 
 export type ContractContext = EthersContractContextV5<
   Erc20Contract,
