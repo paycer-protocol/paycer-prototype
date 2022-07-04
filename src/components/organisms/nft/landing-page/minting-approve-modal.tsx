27f464d62a6555adde09b2582bdcb5feef5b56b3
Julien Scholz <8947616+pikaju@users.noreply.github.com>
2022-07-04 14:27:46 +0200

    PNS-401 Fix build issues

--- src/components/organisms/nft/landing-page/minting-approve-modal.tsx
+++ src/components/organisms/nft/landing-page/minting-approve-modal.tsx
@@ -13,36 +13,33 @@ export interface MintingApproveModalProps {
 }
 
 const MintingApproveModal = ({ amount, publicSaleStarted, alloc, merkleProof, show, onHide }: MintingApproveModalProps) => {
-  const nftPresale = useNftSale('presale')
-  const nftPublicSale = useNftSale('publicSale')
+  // const onMintClicked = useCallback(() => {
+  //   if (nftSale.status === 'idle' && nftSale.info.currentPhase === '') {
+  //     nftSale.buy(amount)
+  //   } else {
+  //     nftPresale.buy(amount, alloc, merkleProof)
+  //   }
+  // }, [nftPresale, nftPublicSale, amount, publicSaleStarted])
 
-  const onMintClicked = useCallback(() => {
-    if (publicSaleStarted) {
-      nftPublicSale.buy(amount)
-    } else {
-      nftPresale.buy(amount, alloc, merkleProof)
-    }
-  }, [nftPresale, nftPublicSale, amount, publicSaleStarted])
-
-  return (
-    <TransactionApproveModal
-      show={show}
-      onHide={() => { nftPresale.resetState(); nftPublicSale.resetState(); onHide() }}
-      onClick={onMintClicked}
-      title={t`Confirm Mint`}
-      btnLabel={t`Mint now`}
-      loading={nftPresale.status === 'loading' || nftPublicSale.status === 'loading'}
-      error={(nftPresale.status === 'error' || nftPublicSale.status === 'error') ? new Error('Something went wrong') : null}
-      success={nftPresale.status === 'success' || nftPublicSale.status === 'success'}
-      successMessage={t`NFT was minted successfully`}
-    >
-      {
-                amount == 1
-                  ? <p className="text-center">{t`You are about to mint an NFT. Are you ready?`}</p>
-                  : <p className="text-center">{t`You are about to mint ${amount} NFTs. Are you ready?`}</p>
-            }
-    </TransactionApproveModal>
-  )
+  // return (
+  //   <TransactionApproveModal
+  //     show={show}
+  //     onHide={() => { nftPresale.resetState(); nftPublicSale.resetState(); onHide() }}
+  //     onClick={onMintClicked}
+  //     title={t`Confirm Mint`}
+  //     btnLabel={t`Mint now`}
+  //     loading={nftPresale.status === 'loading' || nftPublicSale.status === 'loading'}
+  //     error={(nftPresale.status === 'error' || nftPublicSale.status === 'error') ? new Error('Something went wrong') : null}
+  //     success={nftPresale.status === 'success' || nftPublicSale.status === 'success'}
+  //     successMessage={t`NFT was minted successfully`}
+  //   >
+  //     {
+  //       amount == 1
+  //         ? <p className="text-center">{t`You are about to mint an NFT. Are you ready?`}</p>
+  //         : <p className="text-center">{t`You are about to mint ${amount} NFTs. Are you ready?`}</p>
+  //     }
+  //   </TransactionApproveModal>
+  // )
 }
 
 export default MintingApproveModal
