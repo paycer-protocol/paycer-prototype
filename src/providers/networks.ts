dc3470e64fc6e52b41c5cab72b46c77a6bd63fdb
stompudown <39615089+stompudown@users.noreply.github.com>
2022-07-07 17:59:38 +0200

    fix test network (#212)

--- src/providers/networks.ts
+++ src/providers/networks.ts
@@ -4,54 +4,54 @@ import { rpcUrls } from './rpcs'
 import ChainId from './chain-id'
 
 export interface INetworkProvider {
-    [chainId: number]: {
-        chainId: string
-        chainName: string
-        color?: string,
-        nativeCurrency: {
-            name: string
-            symbol: string
-            decimals: number
-        }
-        rpcUrls: string[]
-        blockExplorerUrls: string[],
-        getExplorerAddressLink: any,
-        nativeWrappedTokenInfo: {
-            chainId: ChainId
-            contractAddress: string
-            decimals: number
-            symbol: string
-            name: string
-        },
-        multicallAddress?: string
+  [chainId: number]: {
+    chainId: string
+    chainName: string
+    color?: string,
+    nativeCurrency: {
+      name: string
+      symbol: string
+      decimals: number
     }
+    rpcUrls: string[]
+    blockExplorerUrls: string[],
+    getExplorerAddressLink: any,
+    nativeWrappedTokenInfo: {
+      chainId: ChainId
+      contractAddress: string
+      decimals: number
+      symbol: string
+      name: string
+    },
+    multicallAddress?: string
+  }
 }
 
 export const testNetNetworks: INetworkProvider = {
-    [ChainId.Mumbai]: {
-        chainId: '0x13881',
-        chainName: 'Polygon - Testnet',
-        nativeCurrency: {
-            name: 'Polygon (MATIC Mumbai)',
-            symbol: 'MATIC',
-            decimals: 18,
-        },
-        rpcUrls: [rpcUrls[ChainId.Mumbai]],
-        blockExplorerUrls: [explorers[ChainId.Mumbai]],
-        getExplorerAddressLink: Mumbai.getExplorerAddressLink,
-        nativeWrappedTokenInfo: {
-            chainId: ChainId.Mumbai,
-            contractAddress: "##todo##",
-            decimals: 18,
-            symbol: 'WMATIC',
-            name: 'Wrapped Matic'
-        },
-        multicallAddress: '0xe9939e7Ea7D7fb619Ac57f648Da7B1D425832631'
+  [ChainId.Mumbai]: {
+    chainId: '0x13881',
+    chainName: 'Mumbai',
+    nativeCurrency: {
+      name: 'Polygon (MATIC Mumbai)',
+      symbol: 'MATIC',
+      decimals: 18,
     },
+    rpcUrls: [rpcUrls[ChainId.Mumbai]],
+    blockExplorerUrls: [explorers[ChainId.Mumbai]],
+    getExplorerAddressLink: Mumbai.getExplorerAddressLink,
+    nativeWrappedTokenInfo: {
+      chainId: ChainId.Mumbai,
+      contractAddress: '##todo##',
+      decimals: 18,
+      symbol: 'WMATIC',
+      name: 'Wrapped Matic',
+    },
+    multicallAddress: '0xe9939e7Ea7D7fb619Ac57f648Da7B1D425832631',
+  },
 }
 
 export const mainNetProviders: INetworkProvider = {
-    /*
+  /*
     [ChainId.Mainnet]: {
         chainId: '0x1',
         chainName: 'Ethereum',
@@ -74,33 +74,38 @@ export const mainNetProviders: INetworkProvider = {
         multicallAddress: '0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696'
     },
      */
-    [ChainId.Polygon]: {
-        chainId: '0x89',
-        chainName: 'Polygon',
-        color: '#8247E5',
-        nativeCurrency: {
-            name: 'Matic',
-            symbol: 'MATIC',
-            decimals: 18,
-        },
-        rpcUrls: [rpcUrls[ChainId.Polygon]],
-        blockExplorerUrls: [explorers[ChainId.Polygon]],
-        getExplorerAddressLink: Polygon.getExplorerAddressLink,
-        nativeWrappedTokenInfo: {
-            chainId: ChainId.Polygon,
-            contractAddress: "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270",
-            decimals: 18,
-            symbol: 'WMATIC',
-            name: 'Wrapped Matic'
-        },
-        multicallAddress: '0x275617327c958bD06b5D6b871E7f491D76113dd8'
-    }
+  [ChainId.Polygon]: {
+    chainId: '0x89',
+    chainName: 'Polygon',
+    color: '#8247E5',
+    nativeCurrency: {
+      name: 'Matic',
+      symbol: 'MATIC',
+      decimals: 18,
+    },
+    rpcUrls: [rpcUrls[ChainId.Polygon]],
+    blockExplorerUrls: [explorers[ChainId.Polygon]],
+    getExplorerAddressLink: Polygon.getExplorerAddressLink,
+    nativeWrappedTokenInfo: {
+      chainId: ChainId.Polygon,
+      contractAddress: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
+      decimals: 18,
+      symbol: 'WMATIC',
+      name: 'Wrapped Matic',
+    },
+    multicallAddress: '0x275617327c958bD06b5D6b871E7f491D76113dd8',
+  },
+}
+
+export const allNetProviers: INetworkProvider = {
+  ...mainNetProviders,
+  ...testNetNetworks,
 }
 
 export const infoChartProviders: INetworkProvider = {
-    [ChainId.Polygon]: mainNetProviders[ChainId.Polygon]
+  [ChainId.Polygon]: mainNetProviders[ChainId.Polygon],
 }
 
 export const chainedNetworkProvider = {
-    ...mainNetProviders,
+  ...mainNetProviders,
 }
