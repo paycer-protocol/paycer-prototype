52db8509c889605d340cf0535924a0cabad7d05f
Lukas Gurschler <lukas.gurschler@gmail.com>
2022-07-26 01:50:54 +0200

    css fix

--- src/pages/nft/detail/[pid]/index.tsx
+++ src/pages/nft/detail/[pid]/index.tsx
@@ -2,31 +2,27 @@ import React from 'react'
 import Link from "next/link"
 import { t } from '@lingui/macro'
 import { ChevronLeft } from '@styled-icons/bootstrap'
-import PageHeader from '@components/molecules/page-header'
 import NftDetailContextProvider from 'context/nft-detail-context'
 import Layout from '@components/organisms/layout'
 import NftDetail from '@components/organisms/nft/detail'
 import Icon from '@components/atoms/icon'
 
-
 export default function NftOverviewPage() {
   return (
    <Layout>
      <div className="container mt-3">
-       <PageHeader>
-         <div className="row align-items-center position-relative" style={{ zIndex: 1 }}>
-             <Link href="/nft/my-nfts">
-                 <a href="#!" title={t`Back to NFT Overview`} className="d-flex align-items-center">
-                     <Icon
-                         component={ChevronLeft}
-                         size={18}
-                         color="white"
-                     />
-                     <span className="text-uppercase ps-3 fw-bold">{t`Back`}</span>
-                 </a>
-             </Link>
-         </div>
-       </PageHeader>
+       <div className="row align-items-center position-relative mb-4" style={{ zIndex: 1 }}>
+         <Link href="/nft/my-nfts">
+           <a href="#!" title={t`Back to NFT Overview`} className="d-flex align-items-center">
+             <Icon
+               component={ChevronLeft}
+               size={18}
+               color="white"
+             />
+             <span className="text-uppercase ps-3 fw-bold">{t`Back`}</span>
+           </a>
+         </Link>
+       </div>
        <div>
          <NftDetailContextProvider>
            <NftDetail />
